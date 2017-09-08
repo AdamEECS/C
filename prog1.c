fad956453048083c27ce2da261625a99742572a5
suzumiya <jrgwyz@163.com>
2017-09-08 22:48:27 +0800

    add

--- prog1.c
+++ prog1.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
+
 int main(void) {
-    printf("Programming is fun.\n");
+    // declare variables
+    int sum;
+    sum = 50 + 25;
+
+    printf("The sum is %i.\n", sum);
+
     return 0;
 }
